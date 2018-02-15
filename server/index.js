c4106055cbb77a0817951b6419d0da45befc3975
nicholasagneta <nkagneta@gmail.com>
2018-02-15 10:46:48 -0500

    express

--- server/index.js
+++ server/index.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+var express = require('express')
+var app = express()
+
+// respond with "hello world" when a GET request is made to the homepage
+
+app.use(function(req, res, next){
+    res.write('This is provided by newpaltz.edu\r\n')
+    next();
+});
+app.get('/hello', function (req, res) {
+  res.write('World');
+  res.end();
+});
+app.get('/goodby', function (req, res) {
+    res.write('New Paltz');
+    res.end();
+});
+
+app.listen(8080);
