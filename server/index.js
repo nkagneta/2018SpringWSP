9c723cb9f1c60b21579d50c8477129960056c854
nicholasagneta <nkagneta@gmail.com>
2018-02-26 20:04:28 -0500

    Update from Visual Code

--- server/index.js
+++ server/index.js
@@ -1,19 +1,12 @@
 var express = require('express')
 var app = express()
 
-// respond with "hello world" when a GET request is made to the homepage
+const servername = "localhost";
+const port = 8080;
 
-app.use(function(req, res, next){
-    res.write('This is provided by newpaltz.edu\r\n')
-    next();
-});
-app.get('/hello', function (req, res) {
-  res.write('World');
-  res.end();
-});
-app.get('/goodby', function (req, res) {
-    res.write('New Paltz');
-    res.end();
-});
+const simple = require('./simpleController');
 
-app.listen(8080);
+
+app.use('/simple', simple).listen(port);
+
+console.log("running on http://" + servername + ":" + port)
---
+++ src/Model.ts
@@ -0,0 +1,5 @@
+
+export class Alerts {
+    
+    public Messages: string[];
+}
