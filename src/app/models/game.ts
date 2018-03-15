180f313b8e465c682a9c489cdf9dbed548b4fb54
nicholasagneta <nkagneta@gmail.com>
2018-03-15 10:48:00 -0400

    Game Components Before Spring Break

---
+++ src/app/models/game.ts
@@ -0,0 +1,23 @@
+
+
+export class Game {
+    MyQuotes: string[] = [
+        "Great minds think alike, but...",
+        "Axis of Evil",
+        "There are two extremes"
+    ];
+    Players: User[] = [];
+    Picture: string = "https://cdn.vox-cdn.com/thumbor/w3toxxZ9sLiDWslw9gJh3vyy56I=/0x0:3674x2800/1200x800/filters:focal(1761x932:2347x1518)/cdn.vox-cdn.com/uploads/chorus_image/image/58571535/usa_today_10588737.1517801738.jpg";
+    PlayedQuotes: Quote[] = [];
+
+}
+
+export class User{
+    Name: string;
+}
+
+export class Quote{
+    Text: string;
+    PlayerName: string;
+    Chosen: boolean = false;
+}
