02e9c874159aa71c908487956f4c2884f708c7de
nicholasagneta <nkagneta@gmail.com>
2018-03-17 19:17:27 -0400

    Fixed Messages

--- src/app/models/game.ts
+++ src/app/models/game.ts
@@ -6,9 +6,13 @@ export class Game {
         "Axis of Evil",
         "There are two extremes"
     ];
-    Players: User[] = [];
-    Picture: string = "https://cdn.vox-cdn.com/thumbor/w3toxxZ9sLiDWslw9gJh3vyy56I=/0x0:3674x2800/1200x800/filters:focal(1761x932:2347x1518)/cdn.vox-cdn.com/uploads/chorus_image/image/58571535/usa_today_10588737.1517801738.jpg";
-    PlayedQuotes: Quote[] = [];
+    Players: User[] = [
+        { Name: 'Nicholas'}
+    ];
+    Picture: string = 'https://cdn.vox-cdn.com/thumbor/w3toxxZ9sLiDWslw9gJh3vyy56I=/0x0:3674x2800/1200x800/filters:focal(1761x932:2347x1518)/cdn.vox-cdn.com/uploads/chorus_image/image/58571535/usa_today_10588737.1517801738.jpg';
+    PlayedQuotes: Quote[] = [
+        { Text: "Great minds think alike, but...", PlayerName: 'Nicholas', Chosen: false}
+    ];
 
 }
 
