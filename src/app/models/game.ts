392a9c3af6efb1c7a59d733e3f3afc3bd1129721
nicholasagneta <nkagneta@gmail.com>
2018-03-29 10:37:16 -0400

    Playable by one person

--- src/app/models/game.ts
+++ src/app/models/game.ts
@@ -7,11 +7,14 @@ export class Game {
         "There are two extremes"
     ];
     Players: User[] = [
-        { Name: 'Nicholas'}
+        { Name: 'Nicholas Agneta'},
+        { Name: 'Arin Hanson'},
+        //{ Name: 'Dan Avidan'},
     ];
+    Dealer: string = "Nicholas Agneta";
     Picture: string = 'https://cdn.vox-cdn.com/thumbor/w3toxxZ9sLiDWslw9gJh3vyy56I=/0x0:3674x2800/1200x800/filters:focal(1761x932:2347x1518)/cdn.vox-cdn.com/uploads/chorus_image/image/58571535/usa_today_10588737.1517801738.jpg';
-    PlayedQuotes: Quote[] = [
-        
+    PlayedQuotes: Quote[] = [   
+        {Text: "That's fake news", PlayerName: 'Arin Hanson', Chosen: false } 
     ];
 
 }
