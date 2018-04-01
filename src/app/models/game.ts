5287d34988a742fd41d6b9f98f193593f9637ee9
nicholasagneta <nkagneta@gmail.com>
2018-03-31 22:50:56 -0400

    Bug Fix for "Playable by one person"

--- src/app/models/game.ts
+++ src/app/models/game.ts
@@ -9,7 +9,7 @@ export class Game {
     Players: User[] = [
         { Name: 'Nicholas Agneta'},
         { Name: 'Arin Hanson'},
-        //{ Name: 'Dan Avidan'},
+        { Name: 'Dan Avidan'},
     ];
     Dealer: string = "Nicholas Agneta";
     Picture: string = 'https://cdn.vox-cdn.com/thumbor/w3toxxZ9sLiDWslw9gJh3vyy56I=/0x0:3674x2800/1200x800/filters:focal(1761x932:2347x1518)/cdn.vox-cdn.com/uploads/chorus_image/image/58571535/usa_today_10588737.1517801738.jpg';
