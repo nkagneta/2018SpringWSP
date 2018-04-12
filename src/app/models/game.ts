be2279df4d7c351090847ecee8df6703bbdd1f9a
nicholasagneta <nkagneta@gmail.com>
2018-04-12 10:48:54 -0400

    More Middleware

--- src/app/models/game.ts
+++ src/app/models/game.ts
@@ -8,7 +8,9 @@ export class Game {
         { Name: 'Dan Avidan', MyQuotes: [] },
     ];
     Dealer: string = "Nicholas Agneta";
-    Picture: string = 'https://cdn.vox-cdn.com/thumbor/w3toxxZ9sLiDWslw9gJh3vyy56I=/0x0:3674x2800/1200x800/filters:focal(1761x932:2347x1518)/cdn.vox-cdn.com/uploads/chorus_image/image/58571535/usa_today_10588737.1517801738.jpg';
+    Picture: {
+        url: string
+    };
     PlayedQuotes: Quote[] = [   
         {Text: "That's fake news", PlayerName: 'Arin Hanson', Chosen: false } 
     ];
